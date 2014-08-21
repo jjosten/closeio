4689bb658be80a053ade9afc12ed21d7d3847dee
jan <jan@pijajo.com>
2014-08-21 18:06:12 +0200

    renamed ENV into Config

--- lib/closeio/base.rb
+++ lib/closeio/base.rb
@@ -3,7 +3,7 @@ module Closeio
 
     include HTTParty
     base_uri   'https://app.close.io/api/v1'
-    basic_auth ENV['CLOSEIO_API_KEY'], ''
+    basic_auth CONFIG['close_io_key'], ''
     headers 'Content-Type' => 'application/json'
     #debug_output $stdout
     format :json
